b8787ccae4a01878461686e71cc0bb6e26f46a9b
CarlosGomes07 <carlosgomes1999@hotmail.com>
2024-12-11 02:51:02 -0300

    feat: creation of layout and loading components

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,15 @@
-import { View, Text} from "react-native";
+import { View, Text } from "react-native";
 
 export default function Index() {
-    return (
-        <View>
-            <Text> Hello React Native!!</Text>
-        </View>
-    )
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text> Hello React Native!!</Text>
+    </View>
+  );
 }
